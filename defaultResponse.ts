bd5412356877379274f0aa54373453ae578470a9
Tim Pequignot <tim@peq.me>
2021-10-04 19:29:08 -0500

    Update default reponse to calculate dates.

--- defaultResponse.ts
+++ defaultResponse.ts
@@ -1,22 +1,24 @@
 
-export function defaultResponse(pageError = ""):Response{
+export function defaultResponse():Response{
+  const startYear = 2002;
+  const currentTime = new Date();
+  const currentYear = currentTime.getFullYear();
   return new Response(`
 <html>
-<head>
-<title>OhCrap.org - 2002 - 2021</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-<H2>OhCrap.org</H2>
-<p>Celebrating 19 years of crap</p>
-</body>
+  <head>
+    <title>OhCrap.org - ${startYear} - ${currentYear}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <H2>OhCrap.org</H2>
+    <p>Celebrating ${currentYear - startYear} years of crap!</p>
+  </body>
 </html>
 `,
   {
     status: 200,
     headers: {
       "content-type": "text/html; charset=UTF-8",
-    },
-    
+    }
   });
 }
