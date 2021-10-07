d2b394cf92979c5a66ebee7bb8abd4a494351d35
Tim Pequignot <tim@peq.me>
2021-10-06 22:18:59 -0500

    Create parcho response, create response folder.

--- response/parcho.ts
+++ response/parcho.ts
@@ -1,17 +1,22 @@
 
-export function defaultResponse():Response{
+export function parchoResponse():Response{
   const startYear = 2002;
   const currentTime = new Date();
   const currentYear = currentTime.getFullYear();
+
+  const title = `OhCrap.org ${startYear} - ${currentYear}`.split("").reverse().join();
+  const headline = `Celebrating ${currentYear - startYear} years of crap!`.split("").reverse().join();
+
+
   return new Response(`
 <html>
   <head>
-    <title>OhCrap.org - ${startYear} - ${currentYear}</title>
+    <title>${title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
-    <H2>OhCrap.org</H2>
-    <p>Celebrating ${currentYear - startYear} years of crap!</p>
+    <H2>gro.parChO</H2>
+    <p>${headline}</p>
   </body>
 </html>
 `,
