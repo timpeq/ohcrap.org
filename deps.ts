66b50604d3d14db09c0232b10bdc5a88fddd090e
Tim Pequignot <tim@peq.me>
2021-10-24 11:45:35 -0500

    Downgrade deps to troubleshoot

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { listenAndServe } from "https://deno.land/std@0.112.0/http/server.ts";
-export { serveFile } from "https://deno.land/std@0.112.0/http/file_server.ts";
-export * as log from "https://deno.land/std@0.112.0/log/mod.ts";
+export { listenAndServe } from "https://deno.land/std@0.110.0/http/server.ts";
+export { serveFile } from "https://deno.land/std@0.110.0/http/file_server.ts";
+export * as log from "https://deno.land/std@0.110.0/log/mod.ts";
