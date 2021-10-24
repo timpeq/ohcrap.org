2298ea93b6687a00e60b214a6a8c4d9c7f8f5844
Tim Pequignot <tim@peq.me>
2021-10-24 11:08:08 -0500

    Upgrade deps, add std/logger

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
-export { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
-export { serveFile } from "https://deno.land/std@0.111.0/http/file_server.ts";
+export { listenAndServe } from "https://deno.land/std@0.112.0/http/server.ts";
+export { serveFile } from "https://deno.land/std@0.112.0/http/file_server.ts";
+export * as log from "https://deno.land/std@0.112.0/log/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,5 +1,7 @@
-import { listenAndServe } from "./deps.ts";
+import { log, listenAndServe } from "./deps.ts";
 import { handleRequest } from "./handleRequest.ts";
+
+log.info("Server starting")
 
 listenAndServe(":8000", (request: Request)=>{
   return handleRequest(request);
