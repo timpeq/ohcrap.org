f79420c324dca36b7b9164ba91f919c2de53d4ee
Tim Pequignot <tim@peq.me>
2021-10-24 21:46:26 -0500

    Log IP of request test

--- handleRequest.ts
+++ handleRequest.ts
@@ -1,9 +1,12 @@
+import { log } from "./deps.ts";
 import { errorResponse } from "./response/error.ts";
 import { defaultResponse } from "./response/default.ts";
 import { staticResponse } from "./response/static.ts";
 
 export async function handleRequest(request: Request): Promise<Response> {
   const { pathname } = new URL(request.url);
+
+  log.info(`Request from ${request.headers.get('x-forwarded-for')} for ${pathname}`);
 
   if (pathname === "/") {
     return defaultResponse();
